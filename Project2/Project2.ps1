b2e3328d82e9bfc0d1df42b0a4a70f903c8f4ba2
uc-kokengna <70075871+uc-kokengna@users.noreply.github.com>
2020-10-20 17:43:19 -0400

    Add files via upload

---
+++ Project2/Project2.ps1
@@ -0,0 +1,37 @@
+
+    function getDSK {
+      Get-CimInstance CIM_DiskDrive  | ForEach-Object {
+    
+      $disk = $_
+      $partitions = "ASSOCIATORS OF " +
+                    "{Win32_DiskDrive.DeviceID='$($disk.DeviceID)'} " +
+                    "WHERE AssocClass = Win32_DiskDriveToDiskPartition"
+    
+        Get-CimInstance -Query $partitions | ForEach-Object {
+        $partition = $_
+        $drives = "ASSOCIATORS OF " +
+                  "{Win32_DiskPartition.DeviceID='$($partition.DeviceID)'} " +
+                  "WHERE AssocClass = Win32_LogicalDiskToPartition"
+    
+        Get-CimInstance -Query $drives | ForEach-Object   {
+          New-Object -Type PSCustomObject -Property @{
+
+            DiskPartition = $partition.Name
+            DriveLetter = $_.DeviceID
+            FileSystem = $_.FileSystem
+            Description = $_.Description
+            DiskSizeGB        = ($_.Size/1GB) -as [float]
+            FreeSpaceGB   = ($_.FreeSpace/1GB) -as [float]
+            VolumeName  = $_.VolumeName
+            DiskModel   = $disk.Model
+            PercentageFree= ($_.FreeSpace/$_.Size*100) -as [float] 
+            Status = $disk.Status
+          }
+        }
+      }
+    }
+}
+
+$DSK= (getDSK | Select-object -Property DriveLetter, DiskPartition, FileSystem, Status, VolumeName, Description, DiskSizeGB, FreeSpaceGB , PercentageFree, DiskModel);
+    
+Write-Host($DSK | Out-GridView );
